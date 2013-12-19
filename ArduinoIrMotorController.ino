ec1cccc60ae4cf899fb627da5547a2a3b02c35aa
Timmo Freudl-Gierke <timmo.gierke@gmail.com>
2013-12-20 00:48:51 +0100

    tiny-core installiert und in der IDE zum fliegen gebracht

---
+++ ArduinoIrMotorController.ino
@@ -0,0 +1,111 @@
+#include <IRremote.h>
+#include <IRremoteInt.h>
+#include <stdlib.h>
+
+// Arduino Pins
+const int RECV_PIN = 2;
+const int ENGINE_FORWARD_PIN = 0;
+const int ENGINE_BACKWARD_PIN = 1;
+const int LIGHT_PIN = 3;
+
+// Remote Key Values
+const unsigned long KEY_PLAY_PAUSE = 0x77E12084;
+const unsigned long KEY_FORWARD = 0x77E1E084;
+const unsigned long KEY_BACKWARD = 0x77E11084;
+const unsigned long KEY_UP = 0x77E1D084;
+const unsigned long KEY_DOWN = 0x77E1B084;
+const unsigned long KEY_MENU = 0x77E14084;
+const unsigned long FOLLOWUP_KEYPRESS = 0xFFFFFFFF;
+
+const int MAX_VELOCITY = 7;
+const int MAX_OUTPUT_LEVEL = 255;
+
+// variables
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+
+unsigned long key;
+unsigned long lastKey = 0;
+
+bool light = false;
+
+int enginePace = 0; // -MAX_VELOCITY .. 0 .. MAX_VELOCITY
+int engineVelocity = 0; // abs(speed) -> 0 .. MAX_VELOCITY
+int engineOutputPinLevel = 0;
+int engineRoute = 0; // -1, 1
+
+void setup() {
+//  Serial.begin(9600);
+  irrecv.enableIRIn(); // Start the receiver
+  
+  pinMode(ENGINE_FORWARD_PIN, OUTPUT);
+  pinMode(ENGINE_BACKWARD_PIN, OUTPUT);
+}
+
+void loop() {
+  if (irrecv.decode(&results)) {
+    // Serial.println(results.value, HEX);
+    key = results.value;
+
+    if (key == FOLLOWUP_KEYPRESS) {
+      key = lastKey;
+    }
+
+    switch(key) {
+    case KEY_PLAY_PAUSE:
+//      Serial.println("KEY_PLAY_PAUSE");
+      enginePace = 0;
+      break;
+    case KEY_FORWARD:
+//      Serial.println("KEY_FORWARD");
+      enginePace < MAX_VELOCITY ? enginePace++ : enginePace;
+      break;
+    case KEY_BACKWARD:
+//      Serial.println("KEY_BACKWARD");
+      enginePace > -MAX_VELOCITY ? enginePace-- : enginePace;
+      break;
+    case KEY_MENU:
+//      Serial.println("KEY_MENU");
+      light = !light;
+      break;
+    }
+
+    engineVelocity = abs(enginePace);
+    engineRoute = enginePace >> 7;
+    engineOutputPinLevel = (MAX_OUTPUT_LEVEL * engineVelocity ) / MAX_VELOCITY;
+
+    int  forwardLevel = (1+engineRoute) * engineOutputPinLevel;
+    int backwardLevel = -1 * engineRoute * engineOutputPinLevel;   
+    analogWrite(ENGINE_FORWARD_PIN, forwardLevel ); 
+    analogWrite(ENGINE_BACKWARD_PIN, backwardLevel); 
+
+    //    Serial.print("enginePace: "); 
+    //    Serial.println(enginePace);
+    //    Serial.print("engineVelocity: "); 
+    //    Serial.println(engineVelocity);
+    //    Serial.print("engineRoute: "); 
+    //    Serial.println(engineRoute);    
+    //    Serial.print("forwardLevel: "); 
+    //    Serial.println(forwardLevel);
+    //    Serial.print("backwardLevel: "); 
+    //    Serial.println(backwardLevel);
+    //    Serial.println("\n");
+
+    analogWrite(LIGHT_PIN, 255 * light);
+
+    lastKey = key;
+    irrecv.resume(); // Receive the next value
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
--- Makefile
+++ Makefile
@@ -4,11 +4,13 @@
 BOARD_TAG    = attiny85
 MCU = attiny85
 F_CPU = 8000000
-#ALTERNATE_CORE = tiny8
+ALTERNATE_CORE = tiny8
+#ARDUINO_CORE_PATH = /Applications/Arduino.app/Contents/Resources/Java/hardware/attiny/cores/attiny
 ALTERNATE_CORE_PATH = /Applications/Arduino.app/Contents/Resources/Java/hardware/attiny
+#ALTERNATE_CORE_PATH = /Applications/Arduino.app/Contents/Resources/Java/hardware/tiny/avr/cores
 ARDUINO_VAR_PATH = /Applications/Arduino.app/Contents/Resources/Java/hardware/attiny/variants/tiny8
 
-ARDUINO_LIBS = IRremote
+ARDUINO_LIBS = IRremote_attiny85
 
 
 include /Users/timmo/Coding/github/Arduino-Makefile/arduino-mk/Arduino.mk
